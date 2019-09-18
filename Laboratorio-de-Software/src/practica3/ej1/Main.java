af1e284e21ac72f19f14d74a9dce92284194cc03
NuriCz <nuria.czajko@gmail.com>
2019-09-18 19:32:19 -0300

    Se hizo ej1 prac3

---
+++ Laboratorio-de-Software/src/practica3/ej1/Main.java
@@ -0,0 +1,23 @@
+package practica3.ej1;
+
+import java.util.Iterator;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push("hola");
+        s.push("chau");
+        Iterator si = s.iterator();
+        while(si.hasNext()){
+            System.out.println(si.next());
+        }
+        while(!s.isEmpty()){
+            System.out.println(s.pop());
+        }
+        
+    }
+}
